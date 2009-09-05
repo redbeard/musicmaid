2f628add7ab42f7e712aa2b4a9bac62df20b1a11
Tal Rotbart <nas@nasgul.(none)>
2009-09-05 15:43:21 +1000

    Some changes.

--- lib/info.rb
+++ lib/info.rb
@@ -1,4 +1,27 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+class Info
+  def self.diff(left, right, *attrs)
+    results = {}
 
-puts "Hello World"
+    attrs.each do | attribute |
+      left_val = left.send(attribute.to_sym)
+      right_val = right.send(attribute.to_sym)
+      results[attribute.to_sym] = { :left => left_val, :right => right_val } unless (left_val.eql?(right_val))
+    end
+
+    return results
+  end
+
+  def self.print_diff(what, left, right, *attrs)
+    diffs = Info.diff(left, right, attrs)
+
+    if (diffs.length > 0)
+      puts "#{what}"
+
+      diffs.each_pair do | k, v |
+        puts "#{k.to_s.center(20)}|#{v[:left].to_s.center(100)}|#{v[:right].to_s.center(100)}"
+      end
+
+    end
+  end
+
+end
